32cd40c8c2a5eb2f02fc916b3a172c8d4c25781d
rnorsejs <rnorsejs@gmail.com>
2023-07-11 21:12:35 +0300

    minor changes

--- 2-server/phonebook/src/App.jsx
+++ 2-server/phonebook/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     axios
       .get("http://localhost:3001/persons")
       .then((response) => setPersons(response.data));
-  });
+  }, []);
 
   const handleNameChange = (e) => setNewName(e.target.value);
   const handlePhoneChange = (e) => setNewPhone(e.target.value);
